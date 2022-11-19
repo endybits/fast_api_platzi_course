8672f7fe4d9f462857ff8f829ad0e1ca59d9b622
endybits <endyb.urbanista@gmail.com>
2022-11-19 11:48:16 -0500

    Deprecate Path Operation

--- main.py
+++ main.py
@@ -154,7 +154,8 @@ async def create_person(person: Person = Body(...)):
 @app.get(
     path='/person/detail',
     status_code=status.HTTP_200_OK,
-    tags=['Person']
+    tags=['Person'],
+    deprecated=True
 )
 async def detail_person(
     name: Optional[str] = Query(
